d1e57a706616586ea07c5f476f0a263ddae1d720
Chinh Le <chinh.le11@gmail.com>
2019-07-30 14:31:30 -0400

    setup project, init app and create app components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class="container">
-    
-    </div>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
 </template>
-
 <script>
-    export default {
-        
-    }
+import QuoteGrid from "./components/QuoteGrid.vue";
+export default {
+  data: function() {
+    return {
+      quotes: ["Just a Quote to see something", "Just another quote"],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
+  }
+};
 </script>
-
 <style>
 </style>
