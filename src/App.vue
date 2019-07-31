817404b89e02b9f25789d3b81e60c78689ffaeb2
Chinh Le <chinh.le11@gmail.com>
2019-07-31 09:42:37 -0400

    allowing for quote deletion

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <!-- <app-new-quote @quoteAdded="newQuote($event)"></app-new-quote> -->
     <!-- OR implicitly -->
     <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="removeQuote"></app-quote-grid>
     <div class="row">
       <div class="col-sm-12 text-center">
         <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
@@ -24,6 +24,9 @@ export default {
   methods: {
     newQuote(quote) {
       this.quotes.push(quote);
+    },
+    removeQuote(index) {
+      this.quotes.splice(index, 1);
     }
   },
   components: {
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="row">
-    <app-quote v-for="(quote, index) in quotes" :key="index">{{ quote }}</app-quote>
+    <!-- KIM: since app-quote tag is not appended to the DOM, hence it won't get reacted to the click event -->
+    <!-- <app-quote v-for="(quote, index) in quotes" :key="index" @click="deleteQuote(index)">{{ quote }}</app-quote> -->
+    <!-- KIM: adding the 'native' modifier allows non-appended tag (app-quote) but its template gets reacted to the click event -->
+    <app-quote
+      v-for="(quote, index) in quotes"
+      :key="index"
+      @click.native="deleteQuote(index)"
+    >{{ quote }}</app-quote>
   </div>
 </template>
 <script>
@@ -8,6 +15,11 @@ import Quote from "./Quote.vue";
 export default {
   props: {
     quotes: Array
+  },
+  methods: {
+    deleteQuote(index) {
+      this.$emit("quoteDeleted", index);
+    }
   },
   components: {
     appQuote: Quote
