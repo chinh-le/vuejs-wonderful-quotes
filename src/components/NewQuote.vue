1ced681debbf3d26f30e640e329642f0b5ae28d9
Chinh Le <chinh.le11@gmail.com>
2019-07-31 10:24:01 -0400

    finishing touches and state management

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="row">
     <form action>
-      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 fomr-group">
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <label for="inputQuote">Quote</label>
         <textarea name id="inputQuote" rows="3" v-model="quote" class="form-control"></textarea>
       </div>
-      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 fomr-group">
+      <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <!-- click.prevent: to not sending the form automatically as default form behavior -->
         <button @click.prevent="createNew" class="btn btn-primary">Add Quote</button>
       </div>
